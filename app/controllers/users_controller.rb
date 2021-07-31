aa04c12338b79fee307662821ff1b47432d5f0a6
加藤義隆 <yoshitakakato@katouyoshitakanoMacBook-Air.local>
2021-07-31 22:35:30 +0900

    チェックボックス追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
     def index
-        @users = User.all
+        @first_mans = User.where(gender: 'man', grade: '1')
+        @first_womans = User.where(gender: 'woman', grade: '1')
+        @second_mans = User.where(gender: 'man', grade: '2')
+        @second_womans = User.where(gender: 'woman', grade: '2')
+    end
+
+    def create
+        binding.pry
     end
 end
