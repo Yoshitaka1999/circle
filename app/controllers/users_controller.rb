232d6d9c673c0f1e28d792912ef791ac0c869d4b
加藤義隆 <yoshitakakato@katouyoshitakanoMacBook-Air.local>
2021-08-01 01:29:25 +0900

    createメソッド作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,40 @@ class UsersController < ApplicationController
     end
 
     def create
+        checked_first_m = params["first_m"].select { |key, value| value == "1" }.keys
+        max_first_m = params["max_count"]["first_m"].to_i
         binding.pry
+        if max_first_m < checked_first_m.size
+            checked_first_m.sample(max_first_m)
+        else
+            checked_first_m
+        end
+
+        checked_first_w = params["first_w"].select { |key, value| value == "1" }.keys
+        max_first_w = params["max_count"]["first_w"].to_i
+        binding.pry
+        if max_first_w < checked_first_w.size
+            checked_first_w.sample(max_first_w)
+        else
+            checked_first_w
+        end
+
+        checked_second_m = params["second_m"].select { |key, value| value == "1" }.keys
+        max_second_m = params["max_count"]["second_m"].to_i
+        binding.pry
+        if max_second_m < checked_second_m.size
+            checked_second_m.sample(max_second_m)
+        else
+            checked_second_m
+        end
+
+        checked_second_w = params["second_w"].select { |key, value| value == "1" }.keys
+        max_second_w = params["max_count"]["second_w"].to_i
+        binding.pry
+        if max_second_w < checked_second_w.size
+            checked_second_w.sample(max_second_w)
+        else
+            checked_second_w
+        end
     end
 end
